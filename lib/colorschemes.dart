8a6d1b2e4f102e2fea7c2c4ad2d30b182e9effba
E-m-i-n-e-n-c-e <kssakhilraj@gmail.com>
2024-09-01 23:36:53 +0530

    Moved color schemes to a different file

---
+++ lib/colorschemes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const List<Color> brightScheme = [
+  Color.fromRGBO(12, 228, 88, 1),
+  Color.fromRGBO(255, 217, 0, 0.852),
+  Color.fromRGBO(225, 67, 9, 0.851),
+  Color.fromRGBO(10, 8, 176, 1),
+];
+const List<Color> darkScheme = [Colors.black, Colors.white];
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dice_app/gradient_container.dart';
+import 'package:dice_app/colorschemes.dart';
 
-const List<Color> brightScheme = [
-  Color.fromRGBO(12, 228, 88, 1),
-  Color.fromRGBO(255, 217, 0, 0.852),
-  Color.fromRGBO(225, 67, 9, 0.851),
-  Color.fromRGBO(10, 8, 176, 1),
-];
-const List<Color> darkScheme = [Colors.black, Colors.white];
 void main() {
   runApp(const MyApp());
 }
